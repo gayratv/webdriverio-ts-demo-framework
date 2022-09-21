efbeec6bec82e86684ad3bcc6ea9a42f1c474c8a
Gayrat Vlasov <ggvlasov@gmail.com>
2022-09-21 22:13:17 +0300

    тесты на webdriver+typescript запущены

---
+++ 1/locators.ts
@@ -0,0 +1,109 @@
+describe('Locators - Test Scenarios',async ()=>{
+
+    beforeEach(async ()=>{
+        await browser.url('https://demoqa.com/links')
+        
+    })
+    it('CSS example', async () => {
+       
+        const createdLink = await $('#created')
+        const linkResponse = await $('#linkResponse')
+
+        createdLink.scrollIntoView()
+        createdLink.click()
+        linkResponse.scrollIntoView()
+        await expect(linkResponse.getText()).toHaveTextContaining('Link has responded with staus 201 and status text Created')
+        
+    })
+
+    it('CSS by index example(nth-child)', async () => {
+
+        const unauthorizedLink = await $('div#linkWrapper p:nth-child(9) a')
+        const linkResponse = await $('#linkResponse')
+
+        unauthorizedLink.scrollIntoView()
+        unauthorizedLink.click()
+        linkResponse.scrollIntoView()
+        await expect(linkResponse).toHaveTextContaining('Link has responded with staus 401 and status text Unauthorized')
+    })
+
+    it('Link Text(=) example', async () => {
+
+        //To get an anchor element with a specific text in it, query the text starting with an equals (=) sign.
+
+        const noContentLink = await $('=No Content')
+        const linkResponse = await $('#linkResponse')
+
+        noContentLink.scrollIntoView()
+        noContentLink.click()
+        linkResponse.scrollIntoView()
+        await expect(linkResponse).toHaveTextContaining('Link has responded with staus 204 and status text No Content')
+    })
+
+    it('Partial Link Text(*=) example', async () => {
+
+        //To find a anchor element whose visible text partially matches your search value, query it by using *= in front of the query string (e.g. *=driver).
+        const badRequestLink = await $('*=Request')
+        const linkResponse = await $('#linkResponse')
+
+        badRequestLink.scrollIntoView()
+        badRequestLink.click()
+
+        linkResponse.scrollIntoView()
+        await expect(linkResponse).toHaveTextContaining('Link has responded with staus 400 and status text Bad Request')
+    })
+
+    it('Element with certain text(<element>={text}) example', async () => {
+
+        //To find a anchor element whose visible text partially matches your search value, query it by using *= in front of the query string (e.g. *=driver).
+        const forbiddenLink = await $('a=Forbidden')
+        const linkResponse = await $('#linkResponse')
+
+        forbiddenLink.scrollIntoView()
+        forbiddenLink.click()
+
+        linkResponse.scrollIntoView()
+        await expect(linkResponse).toHaveTextContaining('Link has responded with staus 403 and status text Forbidden')
+    })
+
+    it('Element with patial text(<element>*={text}) example', async () => {
+
+        //To find a anchor element whose visible text partially matches your search value, query it by using *= in front of the query string (e.g. *=driver).
+        const movedLink = await $('a*=Mov')
+        const linkResponse = await $('#linkResponse')
+
+        movedLink.scrollIntoView()
+        movedLink.click()
+        
+        linkResponse.scrollIntoView()
+        await expect(linkResponse).toHaveTextContaining('Link has responded with staus 301 and status text Moved Permanently')
+    })
+
+    it('Element with patial text(<element>*={text}) example', async () => {
+
+        //To find a anchor element whose visible text partially matches your search value, query it by using *= in front of the query string (e.g. *=driver).
+
+        const movedLink = await $('a*=Mov')
+        const linkResponse = await $('#linkResponse')
+
+        movedLink.scrollIntoView()
+        movedLink.click()
+
+        linkResponse.scrollIntoView()
+        await expect(linkResponse).toHaveTextContaining('Link has responded with staus 301 and status text Moved Permanently')
+    })
+
+    it('Element with xpath example', async () => {
+
+        //To find a anchor element whose visible text partially matches your search value, query it by using *= in front of the query string (e.g. *=driver).
+        const notFoundLink = await $('//*[@id="invalid-url"]')
+        const linkResponse = await $('#linkResponse')
+
+        notFoundLink.scrollIntoView()
+        notFoundLink.click()
+
+        linkResponse.scrollIntoView()
+        await expect(linkResponse).toHaveTextContaining('Link has responded with staus 404 and status text Not Found')
+    })
+
+})
---
+++ README.md
@@ -0,0 +1,42 @@
+# Webdriver IO + Typescript
+
+<img src="https://repository-images.githubusercontent.com/194183695/eb15e700-a3b5-11e9-8c50-5f68afabec0e" alt="webdriver.io">
+
+> Webdriver IO framework built over Youtube videos to teach the framework basics to anyone interested in this technology.
+
+### 💻 Topics
+
+We reviewed topics like:
+
+- [x] Mocha Structure
+- [x] Web Locators
+- [x] First Test: Async/Await first commands
+- [x] Page Object Model 
+- [x] Selenium-standalone-service
+
+YT Branc
+- [x] Docker Selenium Grid 4
+- [x] Allure reporter
+- [x] Assertions
+- [x] Locators/Selectors
+- [x] Tab Management  
+ 
+- ## 💻 Pre-requisites
+
+Before you use this project you only need to have Node Js installed in your computer.
+
+https://nodejs.org/es/download/
+
+## 🚀 Install the project
+
+Install project dependencies with: npm i 
+
+
+"@wdio/selenium-standalone-service": "^7.24.1",
+
+Необходимо установить JDK (Java ) по ссылке
+
+https://www.oracle.com/java/technologies/downloads/#java8
+
+Запуск:
+wdio run wdio.conf.ts
